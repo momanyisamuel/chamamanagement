11c8b544a56ac8fe0421035e72d429a0356efc6c
mburuwakimani <k2mburu@gmail.com>
2019-05-17 23:53:32 +0300

    Add view and delete routes and models for portfolio are working

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,8 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
-const Portfolio = require('../models/portfolio');
+const Models = require('../models');
 
-router.get("/", (req, res) => res.render('portfolio'));
+
+//show portfolio
+router.get('/', (req,res) => 
+Models.Portfolio.findAll()
+ .then( Portfolio => { 
+    res.render('./portfolio/portfolio', {
+        Portfolio
+    })
+  })
+  .catch(err => {
+    console.log( err);
+  })
+)
+
+//add a Votes
+// router.get("/add", (req, res) => res.render('./chamas/chamas'));
+router.post('/add', (req, res) => {
+
+    let { name, category, assetFlag, description, amount, dateRecorded, refDetails, comment } = req.body;
+
+    Models.Portfolio.create({
+        name, category, assetFlag, description, amount, dateRecorded, refDetails, comment 
+    })
+    .then(Portfolio => res.redirect('/portfolio'))
+    .catch(err => console.log(err))
+})
+
+//delete
+
+router.get('/:id', (req, res) => { 
+    console.log(req.params);
+    // Chama.findOne({where:{ id : req.params.id }}).then({
+    //     var delId = req.params.id
+    // })
+    Models.Portfolio.destroy({
+      where:{ id : req.params.id}
+    })
+    .then(() => {
+      res.redirect('/portfolio')
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
