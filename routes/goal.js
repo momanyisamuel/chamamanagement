65de6e844f9cd67ccadffcc1ac75842bcdaf799b
mburuwakimani <k2mburu@gmail.com>
2019-05-17 21:38:07 +0300

    Add view and delete routes and models for goals are working

--- routes/goal.js
+++ routes/goal.js
@@ -1,9 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/database');
-const Goal = require('../models/goal');
+const Models = require('../models');
 
-router.get("/", (req, res) => res.render('goal'));
+//show goals
+router.get('/', (req,res) => 
+Models.Goal.findAll()
+ .then( Goal => { 
+    console.log(Goal)
+    res.render('./goal/goal', {
+        Goal
+    })
+  })
+  .catch(err => {
+    console.log( err);
+  })
+)
+
+//add an goal
+
+router.post('/add', (req, res) => {
+
+    let { name, duration, riskRanking } = req.body;
+
+    Models.Goal.create({
+        name, duration, riskRanking
+    })
+    .then(Goal => res.redirect('/goal'))
+    .catch(err => console.log(err))
+})
+
+//delete
+
+
+router.get('/:id', (req, res) => { 
+    console.log(req.params);
+    // Chama.findOne({where:{ id : req.params.id }}).then({
+    //     var delId = req.params.id
+    // })
+    Models.Goal.destroy({
+      where:{ id : req.params.id}
+    })
+    .then(() => {
+      res.redirect('/goal')
+    })
+    .catch(err => console.log(err))
+})
 
 
 module.exports = router;
